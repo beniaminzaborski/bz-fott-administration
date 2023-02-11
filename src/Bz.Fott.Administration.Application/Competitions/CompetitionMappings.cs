0d51f696bd9c2076ba29ddb2c590c2b24c39371b
Beniamin Zaborski <beniamin.zaborski@gmail.com>
2023-02-11 13:14:03 +0100

    DisableCtorValidation for mapper from entity to DTO

--- src/Bz.Fott.Administration.Application/Competitions/CompetitionMappings.cs
+++ src/Bz.Fott.Administration.Application/Competitions/CompetitionMappings.cs
@@ -8,17 +8,21 @@ internal class CompetitionMappings : Profile
     public CompetitionMappings()
     {
         CreateMap<Competition, CompetitionDto>()
+            .DisableCtorValidation()
             .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id.Value))
             .ForMember(dest => dest.Status, opt => opt.MapFrom(src => src.Status.ToString()));
 
         CreateMap<Distance, DistanceDto>()
+            .DisableCtorValidation()
             .ForMember(dest => dest.Unit, opt => opt.MapFrom(src => src.Unit.ToString()));
 
         CreateMap<CompetitionPlace, CompetitionPlaceDto>()
+            .DisableCtorValidation()
             .ForMember(dest => dest.Latitude, opt => opt.MapFrom(src => src.Localization.Latitude))
             .ForMember(dest => dest.Longitute, opt => opt.MapFrom(src => src.Localization.Longitude));
 
         CreateMap<Checkpoint, CheckpointDto>()
+            .DisableCtorValidation()
             .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id.Value))
             .ForMember(dest => dest.TrackPointAmount, opt => opt.MapFrom(src => src.TrackPoint.Amount))
             .ForMember(dest => dest.TrackPointUnit, opt => opt.MapFrom(src => src.TrackPoint.Unit.ToString()));
