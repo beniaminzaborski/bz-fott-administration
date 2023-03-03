4d35adc177b7d3ab4fd91f4654d721b082463d82
Beniamin Zaborski <beniamin.zaborski@gmail.com>
2023-03-03 18:00:05 +0100

    Slugify message name in MassTransit entity name formatter

--- src/Bz.Fott.Administration.Infrastructure/Messaging/ShortTypeEntityNameFormatter.cs
+++ src/Bz.Fott.Administration.Infrastructure/Messaging/ShortTypeEntityNameFormatter.cs
@@ -1,11 +1,19 @@
 ﻿using MassTransit;
+using System.Text.RegularExpressions;
 
 namespace Bz.Fott.Administration.Infrastructure.Messaging;
 
 internal class ShortTypeEntityNameFormatter : IEntityNameFormatter
 {
+    private const string integrationEventSuffix = "IntegrationEvent";
+
     public string FormatEntityName<T>()
     {
-        return typeof(T).Name.ToString();
+        var messageType = typeof(T).Name;
+        if (messageType.EndsWith(integrationEventSuffix, StringComparison.OrdinalIgnoreCase))
+        {
+            messageType = messageType.Replace(integrationEventSuffix, string.Empty);
+        }
+        return Regex.Replace(messageType, "([a-z])([A-Z])", "$1-$2").ToLower();
     }
 }
